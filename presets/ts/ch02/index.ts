57dfee004835ca079d9e50ab1c0c1269b66ef727
Dai5 <guri.vb.2dx@gmail.com>
2024-04-01 00:31:44 +0900

    ch02 の練習問題の解答を追加

---
+++ presets/ts/ch02/index.ts
@@ -0,0 +1,14 @@
+console.log(
+  `\u{ff28}` +
+    `\u{ff45}` +
+    `\u{ff4c}` +
+    `\u{ff4c}` +
+    `\u{ff4f}` +
+    `\u{ff0c}` +
+    `\u{ff37}` +
+    `\u{ff4f}` +
+    `\u{ff52}` +
+    `\u{ff4c}` +
+    `\u{ff44}` +
+    `\u{ff01}`
+);

47c70faed69fc1ec4ca92611d2d6fdaa7571cc87
Dai5 <guri.vb.2dx@gmail.com>
2024-07-24 02:37:30 +0900

    ch10 ex01 を追加

---
+++ presets/js/ch10/ex01/stats.cjs
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+const sum = (x, y) => x + y;
+const square = (x) => x * x;
+
+exports.mean = (data) => data.reduce(sum) / data.length;
+exports.stddev = function (d) {
+  let m = exports.mean(d);
+  return Math.sqrt(
+    d
+      .map((x) => x - m)
+      .map(square)
+      .reduce(sum) /
+      (d.length - 1)
+  );
+};

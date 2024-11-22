d1d493721c2b05d4b3cc462f1984d5d688a8a88a
Dai5 <guri.vb.2dx@gmail.com>
2024-11-23 02:21:12 +0900

    ex09 追加

--- presets/js/ch15.11-15/ex09/server.js
+++ presets/js/ch15.11-15/ex09/server.js
@@ -12,10 +12,10 @@ const wss = new WebSocketServer({ port });
 
 // ライフゲームのセル (true or false) をランダムに初期化する
 let grid = new Array(ROWS)
-.fill(null)
-.map(() =>
-  new Array(COLS).fill(null).map(() => !!Math.floor(Math.random() * 2))
-);
+  .fill(null)
+  .map(() =>
+    new Array(COLS).fill(null).map(() => !!Math.floor(Math.random() * 2))
+  );
 // 停止状態
 let paused = true;
 
@@ -58,10 +58,52 @@ wss.on("connection", (ws) => {
 function updateGrid(grid) {
   // 新しいグリッドを作成
   const nextGrid = grid.map((arr) => [...arr]);
+
+  // セルの周囲8方向 (上下左右と斜め) のセルの座標
+  const neighbors = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+
   for (let row = 0; row < ROWS; row++) {
     for (let col = 0; col < COLS; col++) {
       // 周囲のセルの生存数を数えて nextGrid[row][col] に true or false を設定する
       //（15.04-10.10の実装を利用）
+
+      let liveNeighbors = 0;
+
+      // 現在探索中のセルの周囲8マスの状態を確認する
+      neighbors.forEach(([dx, dy]) => {
+        const newRow = row + dx;
+        const newCol = col + dy;
+
+        // グリッドの外に行かないように周囲のセルの状態を確認する
+        if (newRow > 0 && newRow < ROWS && newCol > 0 && newCol < COLS) {
+          if (grid[newRow][newCol]) {
+            liveNeighbors++;
+          }
+        }
+      });
+
+      // 現在探索中のセルが生きている場合
+      if (grid[row][col]) {
+        // 周囲のセルが1個以下のときは false
+        // 周囲のセルが4つ以上のときは false
+        if (liveNeighbors < 2 || liveNeighbors > 3) {
+          nextGrid[row][col] = false;
+        }
+      } else {
+        // 周囲のセルが3個のときは
+        if (liveNeighbors === 3) {
+          nextGrid[row][col] = true;
+        }
+      }
     }
   }
   return nextGrid;

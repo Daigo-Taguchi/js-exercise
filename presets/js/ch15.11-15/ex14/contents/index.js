5291c981ddacd05784359627e80ce496a30b9421
Dai5 <41626624+Daigo-Taguchi@users.noreply.github.com>
2024-12-05 01:31:13 +0900

    残りの問題を追加 (#12)

--- presets/js/ch15.11-15/ex14/contents/index.js
+++ presets/js/ch15.11-15/ex14/contents/index.js
@@ -6,11 +6,36 @@ button.addEventListener("click", (e) => {
   e.preventDefault();
   getMessageFromServer();
 });
+
 async function getMessageFromServer() {
+  button.disabled = true;
+
   const messageElement = document.createElement("div");
   messageElement.className = "message";
   messageElement.textContent = "";
   messageContainer.appendChild(messageElement);
 
   // TODO: ここにサーバーとのやり取り等を実装しなさい
+  const message = new EventSource("/message");
+
+  message.addEventListener("message", (event) => {
+    try {
+      const data = JSON.parse(event.data);
+
+      // メッセージ内容を要素に追加
+      messageElement.textContent = data.value;
+      messageContainer.appendChild(messageElement);
+
+      // 最終メッセージでストリームを閉じる
+      if (data.done) {
+        message.close();
+        console.log("Stream closed by server.");
+
+        button.disabled = false;
+      }
+    } catch (err) {
+      console.error("Failed to process server message:", err);
+      button.disabled = false;
+    }
+  });
 }
